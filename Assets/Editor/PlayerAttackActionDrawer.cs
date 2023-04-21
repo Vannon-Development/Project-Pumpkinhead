8d5021392ace191c353b87bd2652f95d88855c49
dglusak <dglusak@gmail.com>
2023-04-21 16:25:48 -0400

    Reversion
    Reverted to original controller

--- Assets/Editor/PlayerAttackActionDrawer.cs
+++ Assets/Editor/PlayerAttackActionDrawer.cs
@@ -1,23 +1,26 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-[CustomPropertyDrawer(typeof(PlayerCharacter.PlayerAction.PlayerActionItem))]
-public class PlayerAttackActionDrawer : PropertyDrawer
+namespace Editor
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(PlayerCharacter.PlayerAction.PlayerActionItem))]
+    public class PlayerAttackActionDrawer : PropertyDrawer
     {
-        EditorGUI.BeginProperty(position, label, property);
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
 
-        Rect[] positions = {
-            new Rect(position.x, position.y, position.width * 0.5f, position.height),
-            new Rect(position.x + position.width * 0.52f, position.y, position.width * 0.3f, position.height),
-            new Rect(position.x + position.width * 0.84f, position.y, position.width * 0.16f, position.height)
-        };
+            Rect[] positions = {
+                new Rect(position.x, position.y, position.width * 0.5f, position.height),
+                new Rect(position.x + position.width * 0.52f, position.y, position.width * 0.3f, position.height),
+                new Rect(position.x + position.width * 0.84f, position.y, position.width * 0.16f, position.height)
+            };
 
-        EditorGUI.PropertyField(positions[0], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.actionCommand)), GUIContent.none);
-        EditorGUI.PropertyField(positions[1], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.animationIndex)), GUIContent.none);
-        EditorGUI.PropertyField(positions[2], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.requiresHit)), GUIContent.none);
+            EditorGUI.PropertyField(positions[0], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.actionCommand)), GUIContent.none);
+            EditorGUI.PropertyField(positions[1], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.animationIndex)), GUIContent.none);
+            EditorGUI.PropertyField(positions[2], property.FindPropertyRelative(nameof(PlayerCharacter.PlayerAction.PlayerActionItem.requiresHit)), GUIContent.none);
 
-        EditorGUI.EndProperty();
+            EditorGUI.EndProperty();
+        }
     }
 }
