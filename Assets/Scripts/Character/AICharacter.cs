76c3f9a52068df611092025418fc0c25a1524ddd
dglusak <dglusak@gmail.com>
2023-04-22 11:05:22 -0400

    Started new character controller
    Initial State structure

---
+++ Assets/Scripts/Character/AICharacter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Character
+{
+    public class AICharacter : BaseCharacter
+    {
+    }
+}
---
+++ Assets/Scripts/Character/MainCharacter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Character
+{
+    public class MainCharacter : BaseCharacter
+    {
+    }
+}
